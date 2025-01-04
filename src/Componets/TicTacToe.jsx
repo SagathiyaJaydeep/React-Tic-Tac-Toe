43c5db4281a0864fc3c69e894276718a906b33b7
sagathiyajaydeep <sagathiyajaydeep66@gmail.com>
2025-01-04 08:42:31 +0530

    chanes done

--- src/Componets/TicTacToe.jsx
+++ src/Componets/TicTacToe.jsx
@@ -2,16 +2,41 @@ import React, { useState } from "react";
 
 function TicTacToe() {
   const [buttonList, setButtonList] = useState(Array(9).fill(""));
-  const [turn, setTurn] = useState("X");
+  const [CurruntPlayer, setCurruntPlayer] = useState("X");
+  const [msg, setMsg] = useState("");
+  const [winner, setWinner] = useState(false);
+  const winChance = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
   const dataSetHandler = (index) => {
     let buttonCopy = [...buttonList];
     if (buttonCopy[index] === "") {
-      let turnCopy = turn;
+      let turnCopy = CurruntPlayer;
       turnCopy = turnCopy === "X" ? "O" : "X";
-      setTurn(turnCopy);
-      buttonCopy[index] = turn;
+      setCurruntPlayer(turnCopy);
+      buttonCopy[index] = CurruntPlayer;
       setButtonList(buttonCopy);
+      checkWinner(CurruntPlayer);
     }
+  };
+
+  const restartGame = () => {
+    setMsg("Start New Game !!");
+    if (buttonList !== "") {
+      setButtonList(Array(9).fill(""));
+    }
+  };
+
+  const checkWinner = (CurruntPlayer) => {
+    console.log(CurruntPlayer);
   };
 
   return (
@@ -19,11 +44,15 @@ function TicTacToe() {
       <div className="mainBox">
         <div className="box">
           {buttonList.map((el, i) => (
-            <button key={i} onClick={() => dataSetHandler(i)}>
+            <button key={i} className="btn" onClick={() => dataSetHandler(i)}>
               {el}
             </button>
           ))}
         </div>
+        <h3>{msg}</h3>
+        <button className="restart-btn" onClick={() => restartGame()}>
+          Restart
+        </button>
       </div>
     </>
   );
